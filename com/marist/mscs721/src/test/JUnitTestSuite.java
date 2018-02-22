fbfce1393ab3e054745255e079398f10aa4cd849
NEHAERSAVADLA <neha.ersavadla1@marist.edu>
2018-02-22 17:13:23 -0500

    Create JUnitTestSuite.java

---
+++ com/marist/mscs721/src/test/JUnitTestSuite.java
@@ -0,0 +1,15 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+    MeetingTest.class,
+    RoomTest.class,
+    RoomSchedulerTest.class
+})
+/**
+ * created by Neha
+ */
+public class JunitTestSuite {
+}
